baad00079801222466772e4922683ae83d2654d8
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-03-22 12:04:28 +0100

    added p tag and benefits element

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -11,9 +11,17 @@ const Benefits = ({ setSelectedPage }: Props) => {
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
-        <div>
+        {/* HEADER */}
+        <div className="md:my-5 md:w-3/5">
           <HText>MORE THAN JUST A GYM</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to get you to your
+            ultimate fitness goals with ease. We provide true care into each and every member.
+          </p>
         </div>
+
+        {/* BENEFITS */}
+        <div className="md:flex items-center justify-between gap-8 mt-5"></div>
       </motion.div>
     </section>
   )
