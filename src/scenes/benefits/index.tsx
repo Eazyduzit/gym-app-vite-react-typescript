8874a4bddda4ad83bf84f906d05a34752bef010d
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-03-22 12:25:33 +0100

    created an array of objects that we will map through

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,6 +3,27 @@ import { SelectedPage } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion } from "framer-motion"
 
+const benefits = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Our gym's are packed with the latest equipment. Whether you prefer cardio, resisitance, free weights or all three, we've got you covered.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Discover best fitness classes with our ultimate guide! Expert recs, class descriptions, and workout tips for an effective sweat session.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Our personal trainers are enthusiastic people who are passionate about helping others reach their fitness goals.",
+  },
+]
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
 }
@@ -21,7 +42,11 @@ const Benefits = ({ setSelectedPage }: Props) => {
         </div>
 
         {/* BENEFITS */}
-        <div className="md:flex items-center justify-between gap-8 mt-5"></div>
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+          {benefits.map((benefit) => (
+            <Benefit />
+          ))}
+        </div>
       </motion.div>
     </section>
   )
