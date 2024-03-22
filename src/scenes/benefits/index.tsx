69aab92940b58a0eea9605a561f91ea74ed1a3b4
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-03-22 13:08:02 +0100

    created container variable with stagger effect and added motion framer to element

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -25,6 +25,13 @@ const benefits: Array<BenefitType> = [
   },
 ]
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+}
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
 }
@@ -43,7 +50,13 @@ const Benefits = ({ setSelectedPage }: Props) => {
         </div>
 
         {/* BENEFITS */}
-        <div className="md:flex items-center justify-between gap-8 mt-5">
+        <motion.div
+          className="md:flex items-center justify-between gap-8 mt-5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
           {benefits.map((benefit: BenefitType) => (
             <Benefit
               key={benefit.title}
@@ -53,7 +66,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           ))}
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   )
