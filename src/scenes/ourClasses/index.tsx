ebeaab3ea7c5daa7d27dd26c4467b15df00013b6
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-03-26 12:07:25 +0100

    created section for our classes, added motion divs, set classes and props

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,4 +1,6 @@
 import { SelectedPage } from "@/shared/types"
+import { motion } from "framer-motion"
+import HText from "@/shared/HText"
 import image1 from "@/assets/image1.png"
 import image2 from "@/assets/image2.png"
 import image3 from "@/assets/image3.png"
@@ -11,7 +13,29 @@ type Props = {
 }
 
 const OurClasses = ({ setSelectedPage }: Props) => {
-  return <div>Our Classes</div>
+  return (
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}>
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </motion.div>
+      </motion.div>
+    </section>
+  )
 }
 
 export default OurClasses
