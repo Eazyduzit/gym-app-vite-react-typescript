d95d16233383071e0c077c495ee5916363b8ae09
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-03-27 15:57:54 +0100

    added section and motion div for contact us page

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,11 +1,18 @@
+import { useForm } from "react-hook-form"
 import { SelectedPage } from "@/shared/types"
+import { motion } from "framer-motion"
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png"
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
 }
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  return <div>ContactUs</div>
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}></motion.div>
+    </section>
+  )
 }
 
 export default ContactUs
