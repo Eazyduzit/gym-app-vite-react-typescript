d9dee5b4eb3fbe4eaebf6deb49f7926a4a5f4652
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-03-27 16:12:22 +0100

    created the header part of page

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form"
 import { SelectedPage } from "@/shared/types"
 import { motion } from "framer-motion"
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png"
+import HText from "@/shared/HText"
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
@@ -10,7 +11,26 @@ type Props = {
 const ContactUs = ({ setSelectedPage }: Props) => {
   return (
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
-      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}></motion.div>
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </motion.div>
+      </motion.div>
     </section>
   )
 }
