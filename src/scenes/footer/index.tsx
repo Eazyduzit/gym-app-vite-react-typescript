305320d1d2a1f8275da18607a62a98fee961fdc6
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-03-28 16:41:42 +0100

    created footer structure, added image and some classes

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,7 +1,33 @@
-type Props = {}
+import Logo from "@/assets/Logo.png"
+import HText from "@/shared/HText"
 
-const Footer = (props: Props) => {
-  return <div>Footer</div>
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p>@ Evogym All rights reserved.</p>
+        </div>
+        <div>
+          <HText>Links</HText>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div>
+          <HText>Contact Us</HText>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
